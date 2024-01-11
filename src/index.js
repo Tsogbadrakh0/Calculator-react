ef217fe7e59f4704c057cab06060e5a08c192c20
tsogbadrakh baatar <23LP4804@iMAC2021-05s-iMac.local>
2024-01-11 12:02:19 +0800

    calcreact

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+import ReactDOM  from "react-dom";
+import "../src/styles/global.css";
+import "../src/styles/display.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
